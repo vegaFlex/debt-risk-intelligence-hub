2d8d559031f378f91607e75b9d8ff7c8872a41fd
vegaFlex <vega_flex@abv.bg>
2026-03-11 10:32:25 +0200

    chore: prepare safe render deployment config

---
+++ build.sh
@@ -0,0 +1,6 @@
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
+if [ "$RENDER_SEED_DEMO_DATA" = "1" ]; then
+  python manage.py seed_demo_data
+fi
