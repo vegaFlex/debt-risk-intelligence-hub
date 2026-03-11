b52c12152b33228d13ca583110c39303f4d90a79
vegaFlex <vega_flex@abv.bg>
2026-03-11 10:47:01 +0200

    fix: repair render build configuration

--- build.sh
+++
@@ -1,6 +0,0 @@
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
-if [ "$RENDER_SEED_DEMO_DATA" = "1" ]; then
-  python manage.py seed_demo_data
-fi
